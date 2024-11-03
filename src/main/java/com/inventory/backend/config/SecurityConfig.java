af9df882bafece4b93706db678102e6c1c94c19f
kmtGryffindor20 <tripathikmt20@gmail.com>
2024-11-03 15:28:36 +0530

    [ADDED] Admin Authentication

---
+++ src/main/java/com/inventory/backend/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.inventory.backend.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                    .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+                    )
+                    .formLogin(Customizer.withDefaults())
+                    .build();
+    }
+
+    @Bean
+	public AuthenticationManager authenticationManager(
+			UserDetailsService userDetailsService,
+			PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+		return new ProviderManager(authenticationProvider);
+	}
+
+    @Bean
+    JdbcUserDetailsManager users(DataSource dataSource, PasswordEncoder passwordEncoder) {
+
+        UserDetails adminUser = User.builder()
+            .username("admin")
+            .password(passwordEncoder.encode("admin"))
+            .roles("ADMIN")
+            .build();
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(adminUser);
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+    
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,6 +16,23 @@ DROP TABLE IF EXISTS customer_order_shipping_info;
 DROP TABLE IF EXISTS customer_orders;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS authorities;
+DROP TABLE IF EXISTS users;
+
+-- Table to store user details
+CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(100) NOT NULL,
+    enabled BOOLEAN NOT NULL
+);
+
+-- Table to store user roles
+CREATE TABLE IF NOT EXISTS authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+);
+
 
 CREATE TABLE IF NOT EXISTS categories (
     category_id BIGINT PRIMARY KEY AUTO_INCREMENT,
