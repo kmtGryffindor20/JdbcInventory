f9cfde97e9b05326a2bfab27aa03220952105d57
kmtGryffindor20 <tripathikmt20@gmail.com>
2024-11-07 16:32:01 +0530

    [UPDATED]

--- src/main/java/com/inventory/backend/controllers/impl/CustomerOrderController.java
+++ src/main/java/com/inventory/backend/controllers/impl/CustomerOrderController.java
@@ -16,8 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
